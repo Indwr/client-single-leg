b564e6beeae7e6fee9c1c243722752b66e4eb498
SKD <surindersingh@Surinders-MacBook-Air.local>
2021-11-07 20:00:34 +0530

    code updated

--- application/modules/Member/controllers/IMPS.php
+++ application/modules/Member/controllers/IMPS.php
@@ -11,7 +11,7 @@ class IMPS extends CI_Controller {
         $this->load->helper(array('user', 'birthdate', 'security', 'email'));
         date_default_timezone_set('Asia/Kolkata');
       // $this->api_token = "623455573391844"; /// Anil Api (A.S Soft Solution - Anil Kumar) 
-       $this->api_token = "192460283949182"; // Sunil Api ( Sunil Kumar)
+       $this->api_token = ""; // Sunil Api ( Sunil Kumar)
          //$this->api_token = "177653283932743"; // Arun Api (Arun Kumar)
         //$this->api_token = "156996308911217"; // Ramanjeet Api
     }
